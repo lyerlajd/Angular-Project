56478e3bb123b9bf37d051add919a07b5c2ff96e
James Lyerla <lyerlajd@gmail.com>
2023-03-09 19:11:24 -0600

    happy with the css now

--- Assignment3/src/app/post-list/post-list.component.ts
+++ Assignment3/src/app/post-list/post-list.component.ts
@@ -7,8 +7,8 @@ import { Component } from "@angular/core";
 
 export class PostListComponent {
   posts = [
-    // {title: "post 1", description: "this is text from post 1"},
-    // {title: "post 2", description: "this is text from post 2"},
-    // {title: "post 3", description: "this is text from post 3"}
+    {title: "post 1", description: "this is text from post 1"},
+    {title: "post 2", description: "this is text from post 2"},
+    {title: "post 3", description: "this is text from post 3"}
   ]
 }
